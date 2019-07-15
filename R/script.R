cd1d932a37d54b0915ed04c779ddf2686ca376f4
Rhian Vieira <rhianmvs@gmail.com>
2019-07-15 16:26:53 -0300

    Adicionado o R/script.R e Modificado o ReadMe.txt

---
+++ R/script.R
@@ -0,0 +1,7 @@
+ls() #para listar objetos no workspace
+
+rm(list=ls()) #para remover
+
+getwd() #qual é a pasta de trabalho
+
+a<-c(5,4,3,2,1)
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+# Este é um repositório da disciplina ENBT 2019 ministrada por Andrea Sánchez-Tapia, Sara Mortara e Diogo Rocha.
