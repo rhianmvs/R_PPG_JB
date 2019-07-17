68c2f0e04d0218e7922378ee3e57567d3d7466e0
Rhian Vieira <rhianmvs@gmail.com>
2019-07-17 09:51:20 -0300

    Exercícios 02, 03 e 04

---
+++ R/exercício_02.R
@@ -0,0 +1,11 @@
+read.csv2("./Data/exercicio_02_modificado.csv")
+exer2<-read.csv2("./Data/exercicio_02_modificado.csv")
+
+uptake<-exer2$uptake
+uptake
+uptake[22:42]
+Quebec_chilled<-uptake[22:42]
+mean(Quebec_chilled)
+
+mean(exer2[exer2$local=="Quebec" & exer2$tratamento=="chilled","uptake"])
+
