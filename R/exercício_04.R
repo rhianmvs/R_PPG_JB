21cb2cd879601c7dd8182a3d59750de13208b81e
Rhian Vieira <rhianmvs@gmail.com>
2019-07-17 22:41:44 -0300

    atualização do scrip do exercicio 4

--- R/exercício_04.R
+++ R/exercício_04.R
@@ -5,3 +5,8 @@ read.delim("./Aulas/analise_de_dados_ENBT_2019-master/aula04/data/0012594-190621
 dados<-read.delim("./Aulas/analise_de_dados_ENBT_2019-master/aula04/data/0012594-190621201848488.csv")
 
 dados$species
+
+
+dados_sp_lat_long <- dados[c("species","decimalLatitude","decimalLongitude")]
+
+write.table(dados_sp_lat_long, file = "./Data/exercicio_04.csv",sep=",",dec=".")
