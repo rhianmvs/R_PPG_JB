96fabfdaced2ae623d28708bad1983c88b6ce71a
Rhian Vieira <rhianmvs@gmail.com>
2019-07-17 22:30:22 -0300

    Enviando scrip e dados recortados do exercício 04

---
+++ R/exercício_04.R
@@ -0,0 +1,7 @@
+read.delim("./Aulas/analise_de_dados_ENBT_2019-master/aula04/data/0012594-190621201848488.csv")
+
+?read.csv
+
+dados<-read.delim("./Aulas/analise_de_dados_ENBT_2019-master/aula04/data/0012594-190621201848488.csv")
+
+dados$species
