8139c485adc752dfe4c5492b3b4121625e6ec0af
Rhian Vieira <rhianmvs@gmail.com>
2019-07-24 15:59:12 -0300

    Alteração da posição do comando de leitura do pacote

--- R/exercício_03.R
+++ R/exercício_03.R
@@ -1,10 +1,10 @@
+library(vegan)
+
 read.csv2("./Data/exercicio_03_modificado.csv")
 
 exer03<-read.csv2("./Data/exercicio_03_modificado.csv", row.names = 1) #Se eu quiser fazer cálculo eu preciso informar que a minha primeira coluna contém nomes
 
 head(exer03)
-
-library(vegan)
 
 indiv_parcela<-colSums(exer03)
 indiv_parcela
